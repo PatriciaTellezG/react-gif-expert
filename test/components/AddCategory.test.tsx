5f845b3493c48c1fe1e1d09893c179a57ce4064b
TELLEZ GONZALEZ Patricia <patricia.tellez@soprasteria.com>
2023-06-14 12:40:00 +0200

    new test in AddCategory test

--- test/components/AddCategory.test.tsx
+++ test/components/AddCategory.test.tsx
@@ -12,7 +12,23 @@ describe('Pruebas en <AddCategory />', () => {
 
         fireEvent.input( input, { target: {value: 'Saitama'} } );
 
-        expect(input.value).toBe('Saitama');
+        expect(input.value ).toBe('Saitama');
+
+    });
+
+    test('debe de llamar onNewCategory si el input tiene un valor', () => { 
+
+        const inputValue = 'Saitama'; //creo el valor que voy a evaluar
+
+        render( <AddCategory onNewCategory={ () => {} } /> ); //se levanta el sujeto de pruebas
+        
+        const input = screen.getByRole('textbox'); //referencias a input y form
+        const form = screen.getByRole('form');
+
+        fireEvent.input( input, { target: {value: inputValue} } );//disparo el primer evento del formulario para establecer el valor
+        fireEvent.submit(form);
+
+        expect (input.value).toBe('');
 
     });
 
