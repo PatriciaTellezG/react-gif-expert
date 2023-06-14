0eae2c58988453622cab3695402c86883c83b867
TELLEZ GONZALEZ Patricia <patricia.tellez@soprasteria.com>
2023-06-14 12:07:46 +0200

    getGifs test

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,4 +1,5 @@
 export const getGifs = async (category) => {
+    
     const url = `https://api.giphy.com/v1/gifs/search?api_key=2hinu24gIiMTJYPprm8mlFIFvV4rOev2&q=${category}&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
